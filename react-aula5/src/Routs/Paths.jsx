8fda33779e7bc61402d946184afd973cfdac5340
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-06-07 20:43:39 -0300

    Trabalhando com rotas e componentes

--- react-aula5/src/Routs/Paths.jsx
+++ react-aula5/src/Routs/Paths.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Produtos from "../pages/Produtos";
+import Header from "../componets/Header";
 
 const Paths = () => {
     return (
         <>
             <BrowserRouter>
+                <Header/>
                 <Routes>
                     <Route path="/" element={<Home/>}></Route>
                     <Route path="/produtos" element={<Produtos/>}></Route>
