67ac556c1ee2c7ba1700a8df4f157b457626e538
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-06-08 23:31:13 -0300

    update

--- react-aula6/src/App.jsx
+++ react-aula6/src/App.jsx
@@ -1,8 +1,15 @@
+import "primereact/resources/themes/lara-dark-indigo/theme.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
+
+
+
+import {Button} from 'primereact/button'
 const App = () => {
   return (
-    <>
-      
-    </>
+  <>
+    <Button label="Submit"/>
+  </>
   );
 }
  
