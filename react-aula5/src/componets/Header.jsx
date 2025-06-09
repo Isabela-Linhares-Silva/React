d878bf8dbe03d38b90763056523fd6817c57a05f
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-06-08 23:09:31 -0300

    inicio de estilização

--- react-aula5/src/componets/Header.jsx
+++ react-aula5/src/componets/Header.jsx
@@ -1,18 +1,42 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+    background-color: darkslategrey;
+    padding: 0 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: Arial, Helvetica, sans-serif;
+    & h1{
+        color: #FFFFFF;
+    }
+    & nav ul{
+        display: flex;
+        gap: 30px;
+        list-style: none;
+        & li a{
+            color: #FFFFFF80;
+            text-decoration: none;
+            font-size: 15px;
+            &:hover, &.active{
+                color: #FFFFFF;
+            }
+        }
+    }
+`;
 
 const Header = () => {
     return (
-        <>
-            <header>
-                <h1>Logo</h1>
-                <nav>
-                    <ul>
-                        <li><Link to={"/"}>Home</Link></li>
-                        <li><Link to={"Produtos"}>Produtos</Link></li>
-                    </ul>
-                </nav>
-            </header>
-        </>
+        <HeaderContainer>
+            <h1>Logo</h1>
+            <nav>
+                <ul>
+                    <li><NavLink to={"/"}>Home</NavLink></li>
+                    <li><NavLink to={"Produtos"}>Produtos</NavLink></li>
+                </ul>
+            </nav>
+        </HeaderContainer>
     );
 }
  
