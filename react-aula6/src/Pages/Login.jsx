c609ea0a414fccee028cf504d83255e43c7e61fc
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-06-15 11:56:31 -0300

    conhecendo useForm

--- react-aula6/src/Pages/Login.jsx
+++ react-aula6/src/Pages/Login.jsx
@@ -2,30 +2,43 @@ import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 import { Button } from 'primereact/button';
-
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 const Login = () => {
+    const [mostrarSenha,setMostrarSenha]= useState (false);
+    const {register,handleSubmit}= useForm();
+    function logar(dados){
+        console.log(dados)
+    }
     return (
         <>
             <div className=' h-screen flex align-items-center justify-content-center px-3' style={{backgroundColor: 'var(--indigo-800)',}}>
-                <form style={{backgroundColor: 'var(--gray-900)'}} className='col-12 md:col-3 p-3 border-round-md' >
+                <form onSubmit={handleSubmit(logar)} style={{backgroundColor: 'var(--gray-900)'}} className='col-12 md:col-3 p-3 border-round-md' >
 
-                    <h3 style={{color: 'var(--indigo-100)'}}>Seja Bem-Vind@</h3>
+                    <h2 style={{color: 'var(--indigo-100)'}} className='text-center '>Seja Bem-Vind@</h2>
                     <label htmlFor="email" className='block uppercase font-bold text sm' style={{color: 'var(--indigo-50)'}}>Email</label>
                     <InputText 
                         id='email' 
                         type='email' 
                         placeholder='email@email.com' 
                         className=' mb-3 w-full'
+                        {...register('email',{required:true})}
                     />
                     <label htmlFor="senha" className='block uppercase font-bold text sm' style={{color: 'var(--indigo-50)'}}>Senha</label>
                     <div className='mb-3'>
                         <IconField>
-                            <InputIcon className="pi pi-eye"/>
+                            <InputIcon 
+                            className={`pi ${mostrarSenha ? 'pi-eye' : 'pi-eye-slash'}`}
+                            onClick={() => setMostrarSenha(!mostrarSenha)}
+                            style={{ cursor: 'pointer' }}
+                            />
                             <InputText 
                             id='senha'
-                            type='senha' 
+                            type={mostrarSenha? 'text' : 'password'} 
                             placeholder='*********'
-                            className='w-full'/>
+                            className='w-full'
+                            {...register('senha',{required:true})}
+                            />
                         </IconField>
                     </div>
                     <Button label="Entrar" 
